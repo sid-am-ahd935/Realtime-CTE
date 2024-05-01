06d2945c41318cad74044c9dcffd8406cdd15330
Aman Ahmed Siddiqui <aasiddiqui40@gmail.com>
2024-05-01 08:27:14 +0530

    Removed console.logs in production

--- app.js
+++ app.js
@@ -7,28 +7,29 @@ const server = createServer(app);
 const io = new Server(server);
 const port = process.env.PORT || 3000;
 const domain = process.env.RAILWAY_PUBLIC_DOMAIN || "http://localhost"
+const DEBUG = false;
 
 app.get("/", (req, res) => {
-  console.log("'/' path requested");
+  if(DEBUG) console.log("'/' path requested");
   return res.sendFile(__dirname + "/index.html");
 });
 
 let text = "";
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  if(DEBUG) console.log("a user connected");
   socket.emit("to_client", text);
 
   socket.on("from_client", (msg) => {
     text = msg;
     socket.broadcast.emit("to_client", text);
-    // console.log("message: " + text);
+    if(DEBUG)  console.log("message: " + text);
   });
   socket.on("disconnect", () => {
-    console.log("user disconnected");
+    if(DEBUG) console.log("user disconnected");
   });
 });
 
 server.listen(port, () => {
-  console.log(`server running at ${domain}:${port}`);
+  if(DEBUG) console.log(`server running at ${domain}:${port}`);
 });
